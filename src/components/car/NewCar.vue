eee801c0e4b267cfca0eb8ea74e25a7dcbd809a6
jason.gong <jason.gong@orchestrallinc.cn>
2018-09-04 09:46:17 +0800

    城市选择两级联动

--- src/components/car/NewCar.vue
+++ src/components/car/NewCar.vue
@@ -9,7 +9,7 @@
       date-format="{value} 日"
     />
     <wv-popup
-      :visible.sync="isShowSelectPronicePop"
+      :visible.sync="isShowSelectProniceShortPop"
       :close-on-click-mask="true"
     >
       <div class="proviceShort">
@@ -19,12 +19,35 @@
         </div>
       </div>
     </wv-popup>
-    <!--<wv-popup>-->
-      <!--<div class="proviceShortItem" v-for="(item,index) in pacList" @click="clickproviceShort(item)"-->
-           <!--:key="index" :class="{'isNull' : index+1 === listProniceShort.length}">-->
-        <!--<span>{{item}}</span>-->
-      <!--</div>-->
-    <!--</wv-popup>-->
+    <wv-popup
+      :visible.sync="isShowSelectProvicePop"
+      :close-on-click-mask="true"
+    >
+      <div class="proviceShort">
+        <div class="proviceShortItem" v-for="(item,index) in pacList" @click="clickSelectProvice(item)"
+             :key="index">
+          <span style="overflow: hidden;font-size: 10px;white-space: nowrap;text-overflow: clip">{{item.name}}</span>
+        </div>
+        <div class="proviceShortItem" v-for="index in 5-pacList.length%5" :class="{'isNull' : index === index}"></div>
+      </div>
+    </wv-popup>
+
+    <wv-popup
+      :visible.sync="isShowSelectCityPop"
+      :close-on-click-mask="true"
+    >
+      <div class="proviceShort">
+        <div class="proviceShortItem" v-for="(item,index) in currentSelectCityList" @click="clickSelectCity(item)"
+             :key="index">
+          <span style="overflow: hidden;font-size: 10px;white-space: nowrap;text-overflow: clip">{{item.name}}</span>
+        </div>
+        <div class="proviceShortItem" :class="{'isBack' : true}" @click="clickSelectCityBack">
+          <span style="overflow: hidden;font-size: 10px;white-space: nowrap;text-overflow: clip;color: #ffffff">返回</span>
+        </div>
+        <div v-show="(currentSelectCityList.length+1)%5!=0" class="proviceShortItem"
+             v-for="index in 5-(currentSelectCityList.length+1)%5" :class="{'isNull' : index === index}"></div>
+      </div>
+    </wv-popup>
     <div class="head">
       <div class="carcantainer">
         <img class="icon" src="../../assets/images/icon_tabbar.png"/>
@@ -50,20 +73,28 @@
       <span class="carStatusText">| 违章提醒需要的信息</span>
     </div>
     <div class="CarNoAndCity">
-      <div class="timeItem" @click="isShowSelectPronicePop=true">
+      <div class="timeItem">
         <span>车牌</span>
-        <span>{{carNoProvince}}</span>
+        <div class="carNo">
+          <span>{{carNoProvince}}</span>
+          <img @click="isShowSelectProniceShortPop=true" src="../../assets/images/yellow_arrow_down.png"
+               style="margin-left: 2px;width: 18px;height: 14px;align-self: center"/>
+          <input v-model="carNoNumber" type="text"
+                 style="border:none;outline:medium;border-bottom: 1px solid #e1e1e1;width: 100px"/>
+        </div>
+
       </div>
       <div style="height: 80px;width: 1px;background-color: #eeeeee"></div>
-      <div class="timeItem">
-        <span>当前里程</span>
-        <span>选择里程</span>
+      <div class="timeItem" @click="isShowSelectProvicePop=true">
+        <span>查询城市</span>
+        <span>{{city}}</span>
       </div>
     </div>
   </div>
 </template>
 <script>
   import pacList from '../../assets/pca-code'
+
   export default {
     data() {
       return {
@@ -71,27 +102,48 @@
         date: "选择时间",
         carNoProvince: "苏",
         carNoNumber: "",
-        isShowSelectPronicePop: false,
+        isShowSelectProniceShortPop: false,
         listProniceShort: ["京", "津", "冀", "晋", "蒙", "辽", "吉", "黑", "沪", "苏", "浙", "皖", "闽", "赣", "鲁", "豫", "鄂", "湘", "粤", "桂", "琼", "川", "贵", "云", "渝", "藏", "陕", "甘", "青", "宁", "新", "港", "澳", "台", ""],
-        pacList: []
+        pacList: [],
+        city: "选择城市",
+        isShowSelectProvicePop: false,
+        currentSelectCityList: "",
+        isShowSelectCityPop: false,
       }
     },
     mounted() {
       this.name = this.$route.params.name
     },
     methods: {
+      //选择日期的回调
       onConfirm(value) {
         this.date = this.Utils.getDateFormat1(new Date(value))
       },
       openDatePicker() {
         this.$refs.datePicker.open()
       },
+      //选择车牌省份缩写
       clickproviceShort(item) {
         this.carNoProvince = item
-        this.isShowSelectPronicePop = false
+        this.isShowSelectProniceShortPop = false
       },
       getPacList() {
         this.pacList = pacList
+      },
+      //查询城市选择省份
+      clickSelectProvice(item) {
+        this.isShowSelectProvicePop = false
+        this.currentSelectCityList = item.children
+        this.isShowSelectCityPop = true
+      },
+      clickSelectCity(item) {
+        this.city = item.name
+        this.isShowSelectCityPop = false
+      },
+      clickSelectCityBack(){
+        this.isShowSelectProvicePop = true
+        this.currentSelectCityList = []
+        this.isShowSelectCityPop = false
       }
     },
     created() {
@@ -194,7 +246,14 @@
         &.isNull {
           background-color: #eeeeee;
         }
+        &.isBack{
+          background-color: #10aeff;
+        }
       }
+    }
+    .carNo {
+      display: flex;
+      flex-direction: row;
     }
   }
 </style>

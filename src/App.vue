9442b357962622c758f98e15e35df96c9bd31731
kevin.wang <kevin.wang@orchestrallinc.cn>
2018-09-05 09:47:16 +0800

    kevin

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "main.scss";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
